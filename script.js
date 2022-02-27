8f1ffc90cbe8deacab120f307d0feb635fb9e1a4
Steven Dunn <thevgm01@gmail.com>
2022-02-27 17:31:58 -0600

    Added a seconds clock
    I don't know what I'm doing! :D

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 setInterval(displayClock, 100);
 
 function displayClock() {
-    const time = document.querySelector('#time');
+    const time = document.querySelector("#time");
     let date = new Date();
     let hrs = date.getHours();
     let min = date.getMinutes();
+    let sec = date.getSeconds();
     if (hrs > 12) {
         hrs = hrs - 12
     }
+    /*
     if (hrs < 10) {
-        hrs = "0" + hrs
+        hrs = "0" + hrs;
     }
+    */
     if (min < 10) {
-        min = "0" + min
+        min = "0" + min;
     }
-    time.textContent = hrs + ':' + min;
+    if (sec < 10) {
+        sec = "0" + sec;
+    }
+    time.textContent = hrs + ":" + min;
+    seconds.textContent = ":" + sec;
 }
